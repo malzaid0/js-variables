fcac14f387a95aecd403bfefb5bdca9a6fa3ca25
Mohammed Alzaid <m.al-zaid@live.com>
2020-09-24 12:57:19 +0300

    done

---
+++ story.js
@@ -0,0 +1,4 @@
+let name= "Mohammed"
+let age= 25
+let hobby= "Soccer"
+console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`)
